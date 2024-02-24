acd2d8d0eb0bf7e65631594356f4a752b5635614
purohitBhaskar <bhaskarp@designbytes.org>
2024-02-24 22:39:17 +0530

    overview basic template

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,22 +4,68 @@ import "../styles/Overview.css"
 const Overview = () => {
   return (
     <div className="overview-section">
-      <div id="left-section" className="sections">
+      <div className="sections" id="left-section">
         <div className="left-content" id="top-left">
-          image
+          <img src="" alt="" />
         </div>
-        <div className="left-content" id="bottom-left"></div>
+        <div className="left-content" id="bottom-left">
+          <div className="bottom-left-row-heading" id='energy-heading'>
+            Energy Resources
+          </div>
+          <div className="bottom-left-row" id='energy-source'>
+            <div>Energy Source</div>
+            <div>Operating Hours</div>
+            <div>Power Generated</div>
+            <div>Cost</div>
+          </div>
+          <div className="bottom-left-row" id='solar-left'>
+            <div>Solar</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+          <div className="bottom-left-row" id='wind-left'>
+            <div>Wind</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+          <div className="bottom-left-row" id='biogas-left'>
+            <div>Biogas</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+          <div className="bottom-left-row" id='ESS-left'>
+            <div>ESS</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+          <div className="bottom-left-row" id='gensets-left'>
+            <div>Gensets</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+          <div className="bottom-left-row" id='mains-left'>
+            <div>Mains</div>
+            <div>426 hrs</div>
+            <div>46743 kWh</div>
+            <div>10,000</div>
+          </div>
+        </div>
       </div>
-      <div id="right-section" className="sections">
+      <div className="sections" id="right-section">
         <div className="right-content" id="row1">
           <div className="row1-cards" id="bar-graph"></div>
           <div className="row1-cards" id="total-energy-card"></div>
         </div>
         <div className="right-content" id="row2">
           <div className="cards-vertical">
-            <div className="operating-hours"></div>
-            <div className="mains"></div>
-            <div className="gensets"></div>
+            <div className="card-vertical" id="operating-hours"></div>
+            <div className="card-vertical" id="mains"></div>
+            <div className="card-vertical" id="gensets"></div>
           </div>
           <div className="cards-horizontal">
             <div className="top-cards">
@@ -31,8 +77,16 @@ const Overview = () => {
           </div>
         </div>
         <div className="right-content" id="row3">
-          <div className="power-table"></div>
-          <div className="avg-current"></div>
+          <div className="power-table">
+            <div className="power-table-row" id="power-heading"></div>
+            <div className="power-table-row" id="avg-power-kWh"></div>
+            <div className="power-table-row" id="avg-power-kVA"></div>
+          </div>
+          <div className="avg-current">
+            <div className="avg-current-row" id="avg-current-heading"></div>
+            <div className="avg-current-row" id="avg-voltage-l-l"></div>
+            <div className="avg-current-row" id="avg-voltage-l-n"></div>
+          </div>
         </div>
       </div>
     </div>
