a3b285d7c3008c75cb4077939200f7f7f8bffb27
purohitBhaskar <bhaskarp@designbytes.org>
2024-02-25 01:19:03 +0530

    biogas and mains page template complete

--- src/components/Biogas.jsx
+++ src/components/Biogas.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import "../styles/Biogas.css"
 const Biogas = () => {
   return (
-    <div>Biogas</div>
+    <div className="biogas-container">
+      <div className="biogas-left-section">
+        <div className="biogas-left-section-rows" id='biogas-img'></div>
+        <div className="biogas-left-section-rows" id='biogas-vertical-card'>
+          <div className="biogas-vertical-card1">1</div>
+          <div className="biogas-vertical-card2">1</div>
+          <div className="biogas-vertical-card3">1</div>
+          <div className="biogas-vertical-card4">1</div>
+        </div>
+        <div className="left-section-rows" id='notifications'></div>
+      </div>
+      <div className="biogas-right-section">
+        <div className="biogas-right-row1">
+          <div className="biogas-bar-graph"></div>
+          <div className="biogas-yesterday-stats"></div>
+        </div>
+        <div className="biogas-right-row2">
+          <div className="biogas-power-heading"></div>
+          <div className="biogas-kW"></div>
+          <div className="biogas-kVA"></div>
+        </div>
+        <div className="biogas-right-row3">
+          <div className="biogas-operating-hours"></div>
+          <div className="biogas-total-generation"></div>
+          <div className="biogas-total-utilization"></div>
+          <div className="biogas-total-savings"></div>
+        </div>
+        <div className="biogas-right-row4">
+          <div className="current-voltage-heading"></div>
+          <div className="current-voltage-row1"></div>
+          <div className="current-voltage-row2"></div>
+          <div className="current-voltage-row3"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Mains.jsx
+++ src/components/Mains.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import "../styles/Mains.css"
 const Mains = () => {
   return (
-    <div>Mains</div>
+    <div className="mains-container">
+      <div className="mains-left-section">
+        <div className="mains-left-section-rows" id='mains-img'></div>
+        <div className="mains-left-section-rows" id='mains-vertical-card'>
+          <div className="mains-vertical-card1">1</div>
+          <div className="mains-vertical-card2">1</div>
+          <div className="mains-vertical-card3">1</div>
+          <div className="mains-vertical-card4">1</div>
+        </div>
+        <div className="left-section-rows" id='notifications'></div>
+      </div>
+      <div className="mains-right-section">
+        <div className="mains-right-row1">
+          <div className="mains-bar-graph"></div>
+          <div className="mains-yesterday-stats"></div>
+        </div>
+        <div className="mains-right-row2">
+          <div className="mains-power-heading"></div>
+          <div className="mains-kW"></div>
+          <div className="mains-kVA"></div>
+        </div>
+        <div className="mains-right-row3">
+          <div className="mains-operating-hours"></div>
+          <div className="mains-total-generation"></div>
+          <div className="mains-total-utilization"></div>
+          <div className="mains-total-savings"></div>
+        </div>
+        <div className="mains-right-row4">
+          <div className="current-voltage-heading"></div>
+          <div className="current-voltage-row1"></div>
+          <div className="current-voltage-row2"></div>
+          <div className="current-voltage-row3"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
