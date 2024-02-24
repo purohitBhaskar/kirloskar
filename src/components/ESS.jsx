bf288f8fb35bb20aba899a65cd5973098d554bbe
purohitBhaskar <bhaskarp@designbytes.org>
2024-02-25 02:13:54 +0530

    ess section top left, bottom left and top right templates ready

--- src/components/ESS.jsx
+++ src/components/ESS.jsx
@@ -3,7 +3,35 @@ import "../styles/ESS.css"
 
 const ESS = () => {
   return (
-    <div>ESS</div>
+    <div className="ess-container">
+      <div className="ess-left-section">
+        <div className="ess-top-left"></div>
+        <div className="ess-bottom-left">
+          <div className="ess-stat-cards">
+            <div className="ess-stat-card" id="ess-stat-card-1">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-2">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-3">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-4">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-5">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-6">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-7">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-8">1</div>
+            <div className="ess-stat-card" id="ess-stat-card-9">1</div>
+          </div>
+          <div className="ess-energy-meters">
+            <div className="ess-energy-consumption-meter"></div>
+            <div className="ess-soc-meter"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="ess-right-section">
+        <div className="ess-right-row1">1</div>
+        <div className="ess-right-row2">1</div>
+        <div className="ess-right-row3">1</div>
+        <div className="ess-right-row4">1</div>
+      </div>
+    </div>
   )
 }
 
