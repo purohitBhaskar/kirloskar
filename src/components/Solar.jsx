614439d21dd53d172d020d5c69f7c51d5b9358b5
purohitBhaskar <bhaskarp@designbytes.org>
2024-02-25 00:58:10 +0530

    solar and wind template ready

--- src/components/Solar.jsx
+++ src/components/Solar.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import "../styles/Solar.css"
 
 const Solar = () => {
   return (
-    <div>Solar</div>
+    <div className="solar-container">
+      <div className="solar-left-section">
+        <div className="solar-left-section-rows" id='solar-img'></div>
+        <div className="solar-left-section-rows" id='solar-vertical-card'>
+          <div className="solar-vertical-card1">1</div>
+          <div className="solar-vertical-card2">1</div>
+          <div className="solar-vertical-card3">1</div>
+          <div className="solar-vertical-card4">1</div>
+        </div>
+        <div className="left-section-rows" id='notifications'></div>
+      </div>
+      <div className="solar-right-section">
+        <div className="solar-right-row1">
+          <div className="solar-bar-graph"></div>
+          <div className="solar-yesterday-stats"></div>
+        </div>
+        <div className="solar-right-row2">
+          <div className="solar-power-heading"></div>
+          <div className="solar-kW"></div>
+          <div className="solar-kVA"></div>
+        </div>
+        <div className="solar-right-row3">
+          <div className="solar-operating-hours"></div>
+          <div className="solar-total-generation"></div>
+          <div className="solar-total-utilization"></div>
+          <div className="solar-total-savings"></div>
+        </div>
+        <div className="solar-right-row4">
+          <div className="current-voltage-heading"></div>
+          <div className="current-voltage-row1"></div>
+          <div className="current-voltage-row2"></div>
+          <div className="current-voltage-row3"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Wind.jsx
+++ src/components/Wind.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import "../styles/Wind.css"
 const Wind = () => {
   return (
-    <div>Wind</div>
+    <div className="wind-container">
+      <div className="wind-left-section">
+        <div className="wind-left-section-rows" id='wind-img'></div>
+        <div className="wind-left-section-rows" id='wind-vertical-card'>
+          <div className="wind-vertical-card1">1</div>
+          <div className="wind-vertical-card2">1</div>
+          <div className="wind-vertical-card3">1</div>
+          <div className="wind-vertical-card4">1</div>
+        </div>
+        <div className="left-section-rows" id='notifications'></div>
+      </div>
+      <div className="wind-right-section">
+        <div className="wind-right-row1">
+          <div className="wind-bar-graph"></div>
+          <div className="wind-yesterday-stats"></div>
+        </div>
+        <div className="wind-right-row2">
+          <div className="wind-power-heading"></div>
+          <div className="wind-kW"></div>
+          <div className="wind-kVA"></div>
+        </div>
+        <div className="wind-right-row3">
+          <div className="wind-operating-hours"></div>
+          <div className="wind-total-generation"></div>
+          <div className="wind-total-utilization"></div>
+          <div className="wind-total-savings"></div>
+        </div>
+        <div className="wind-right-row4">
+          <div className="current-voltage-heading"></div>
+          <div className="current-voltage-row1"></div>
+          <div className="current-voltage-row2"></div>
+          <div className="current-voltage-row3"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
