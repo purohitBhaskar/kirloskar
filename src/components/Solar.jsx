6e4f1cf04c37a4febe396199a2cf448970a1b829
purohitBhaskar <bhaskarp@designbytes.org>
2024-02-25 23:11:44 +0530

    solar seciton data

--- src/components/Solar.jsx
+++ src/components/Solar.jsx
@@ -7,17 +7,67 @@ const Solar = () => {
       <div className="solar-left-section">
         <div className="solar-left-section-rows" id='solar-img'></div>
         <div className="solar-left-section-rows" id='solar-vertical-card'>
-          <div className="solar-vertical-card1">1</div>
-          <div className="solar-vertical-card2">1</div>
-          <div className="solar-vertical-card3">1</div>
-          <div className="solar-vertical-card4">1</div>
+          <div className="solar-vertical-card1">
+            <div className="solar-vertical-card1-maintenance">Maintenace</div>
+            <div className="solar-vertical-card1-last-date">Last Date: <br /></div>
+            <div className="solar-vertical-card1-next-due">Due Date: <br /></div>
+
+          </div>
+          <div className="solar-vertical-card2">
+            <div className="solar-vertical-card2-utilization-factor">Utilisation<br />factor</div>
+            <div className="solar-vertical-card2-utilization-factor-value">83%</div>
+            <div className="solar-vertical-card2-power-factor">Power Factor</div>
+            <div className="solar-vertical-card2-power-factor-value">0.6</div>
+          </div>
+          <div className="solar-vertical-card3">
+            <div className="solar-vertical-card3-notifications">Notifications</div>
+            <div className="solar-vertical-card3-alerts">Alerts: <br /></div>
+            <div className="solar-vertical-card3-shutdowns">Shutdowns: <br /></div>
+          </div>
+          <div className="solar-vertical-card4">
+            <div className="solar-vertical-card4-frequency">frequency</div>
+            <div className="solar-vertical-card4-frequency-value">50 Hz</div>
+            <div className="solar-vertical-card4-breaker-status">Breaker Status</div>
+            <div className="solar-vertical-card4-breaker-status-on-off-trip">On/Off/Trip</div>
+          </div>
         </div>
-        <div className="left-section-rows" id='notifications'></div>
+        <div className="left-section-rows" id='notifications'>
+          <div className="solar-notifications-heading">Notifications</div>
+          <div className="solar-notifications-row-1">
+            <div className="fault-code">Fault code</div>
+            <div className="description">Description</div>
+            <div className="severity">Severity</div>
+            <div className="status">Status</div>
+            <div className="date-time">Date & Time</div>
+          </div>
+          <div className="solar-notifications-row-2">
+            <div className="fault-code-value">1234</div>
+            <div className="description-value">Blockage Detected</div>
+            <div className="severity-value">Shutdown</div>
+            <div className="status-value">Open</div>
+            <div className="date-time-value">21/12/23-10:23 AM</div>
+          </div>
+          <div className="solar-notifications-row-3">
+            <div className="fault-code-value">1234</div>
+            <div className="description-value">Blockage Detected</div>
+            <div className="severity-value">Shutdown</div>
+            <div className="status-value">Open</div>
+            <div className="date-time-value">21/12/23-10:23 AM</div>
+          </div>
+        </div>
       </div>
       <div className="solar-right-section">
         <div className="solar-right-row1">
           <div className="solar-bar-graph"></div>
-          <div className="solar-yesterday-stats"></div>
+          <div className="solar-yesterday-stats">
+            <div className="solar-yesterday-stats-heading">Yesterday</div>
+            <div className="solar-yesterday-hours-operated">Hours <br />Operated</div>
+            <div className="solar-yesterday-power-generated">
+              Power <br />Generated <br />(kWh)
+            </div>
+            {/* <div className="solar-yesterday-hours-operated-reading">4</div> */}
+            {/* <div className="solar-yesterday-power-generated-reading">24</div> */}
+          </div>
         </div>
         <div className="solar-right-row2">
           <div className="solar-power-heading"></div>
